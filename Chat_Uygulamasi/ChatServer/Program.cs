dc7ac160ea66821ed25ae59c25a33562802d1421
erkanyurtoglu <158731690+erkanyurtoglu@users.noreply.github.com>
2025-06-29 22:37:49 +0300

    Taslak Front Tasarımı Oluşturuldu ve Veri Gönderme Denemesi Yapıldı.

---
+++ Chat_Uygulamasi/ChatServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+
+namespace ChatServer
+{
+    class Program
+    {
+        static List<Client> _users;
+        static TcpListener _listener;
+
+        static void Main(string[] args)
+        {
+            _users = new List<Client>();
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
+            _listener.Start();
+
+            while (true)
+            {
+                var client = new Client(_listener.AcceptTcpClient());
+                _users.Add(client);
+            }
+        } 
+    }
+}
---
+++ Chat_Uygulamasi/Chat_Uygulamasi/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Chat_Uygulamasi.MVVM.Core;
+using Chat_Uygulamasi.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_Uygulamasi.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public RelayCommand ConnectToServerCommand { get; set; }
+
+        private readonly Server _server;
+
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer());
+        }
+    }
+}
